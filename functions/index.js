75dffaa2d8765350935a260fc0776371b46caa70
Anurag Raina <rainakalhan90@gmail.com>
2021-01-25 06:26:10 +0530

    Complete createUser function

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,5 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -7,8 +8,21 @@ const functions = require('firebase-functions');
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+admin.initializeApp();
 
-exports.createUser = functions.https.onCall((data, context) => {
-	console.log(data);
-	return 'received';
+exports.createUser = functions.region('asia-south1').https.onCall(async (data, context) => {
+	try {
+		const userRecord = await admin.auth().createUser({
+			email: data.email,
+			phoneNumber: `+91${data.mobile}`,
+			password: '123456',
+			displayName: data.name,
+		});
+
+		console.log('Successfully created new user:', userRecord.uid);
+		return userRecord;
+	} catch (error) {
+		console.log('Error creating new user:', error);
+		return error;
+	}
 });
