bd2d1e52f2dc29c7e14c69be87626dee9bbf4506
Anurag Raina <rainakalhan90@gmail.com>
2021-01-25 08:04:12 +0530

    Add fetch all users functionality

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,6 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
 admin.initializeApp();
 
 exports.createUser = functions.region('asia-south1').https.onCall(async (data, context) => {
@@ -23,6 +16,25 @@ exports.createUser = functions.region('asia-south1').https.onCall(async (data, c
 		return userRecord;
 	} catch (error) {
 		console.log('Error creating new user:', error);
+		return error;
+	}
+});
+
+exports.getUsers = functions.region('asia-south1').https.onCall(async (data, context) => {
+	try {
+		const finalUserList = [];
+		const userList = await admin.auth().listUsers();
+		userList.users.forEach(userRecord => {
+			const newUser = {
+				name: userRecord.displayName,
+				email: userRecord.email,
+				mobile: userRecord.phoneNumber,
+			};
+			finalUserList.push(newUser);
+		});
+		return finalUserList;
+	} catch (error) {
+		console.log('Error fetching users:', error);
 		return error;
 	}
 });
