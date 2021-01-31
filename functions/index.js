ab8b276352952d28c80ad49fa38c5dffbe6b8e5a
Anurag Raina <rainakalhan90@gmail.com>
2021-01-31 23:20:31 +0530

    Delete functionality

--- functions/index.js
+++ functions/index.js
@@ -13,28 +13,48 @@ exports.createUser = functions.region('asia-south1').https.onCall(async (data, c
 		});
 
 		console.log('Successfully created new user:', userRecord.uid);
-		return userRecord;
+		return {
+			uid: userRecord.uid,
+			name: userRecord.displayName,
+			email: userRecord.email,
+			mobile: userRecord.phoneNumber,
+		};
 	} catch (error) {
 		console.log('Error creating new user:', error);
 		return error;
 	}
 });
 
-exports.getUsers = functions.region('asia-south1').https.onCall(async (data, context) => {
+exports.getUsers = functions.region('asia-south1').https.onCall(async () => {
 	try {
 		const finalUserList = [];
 		const userList = await admin.auth().listUsers();
 		userList.users.forEach(userRecord => {
-			const newUser = {
-				name: userRecord.displayName,
-				email: userRecord.email,
-				mobile: userRecord.phoneNumber,
-			};
-			finalUserList.push(newUser);
+			if (userRecord.email !== 'admin@project.com') {
+				const newUser = {
+					uid: userRecord.uid,
+					name: userRecord.displayName,
+					email: userRecord.email,
+					mobile: userRecord.phoneNumber,
+				};
+				finalUserList.push(newUser);
+			}
 		});
 		return finalUserList;
 	} catch (error) {
 		console.log('Error fetching users:', error);
+		return error;
+	}
+});
+
+exports.deleteUser = functions.region('asia-south1').https.onCall(async (uid, context) => {
+	try {
+		await admin.auth().deleteUser(uid);
+
+		console.log('Successfully deleted user');
+		return 'Successfully deleted user';
+	} catch (error) {
+		console.log('Error deleting user:', error);
 		return error;
 	}
 });
